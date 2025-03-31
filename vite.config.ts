dceafc6a9a6ff288444b3fd08c821503e5a2fdce
pa <gorgitorelleno@gmail.com>
2025-03-31 15:54:36 -0300

    Update vite.config.ts
    Signed-off-by: pa <gorgitorelleno@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
+// Project: PAPIWEB
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/live/', // ¡papiweb!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    chunkSizeWarningLimit: 1600, // Opcional: para silenciar la advertencia de tamaño
+  }
 });
