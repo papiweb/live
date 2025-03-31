46b86aeed441f6753ecd5534aef6076b08e5d816
pa <gorgitorelleno@gmail.com>
2025-03-31 15:14:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://papiweb/live/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/live/',  // ← esencial 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
