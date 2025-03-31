5f67d991a8f412c1a8ce7ee95fd1726832213b1d
pa <gorgitorelleno@gmail.com>
2025-03-31 15:11:17 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// https://papiweb/live/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
